610301e0e0fc719083689b968b2c289dcd3be194
Tonye2e3 <asd.46563@gmail.com>
2026-08-13 21:40:54 +0800

    調整 Program.cs 的 using 命名空間
    移除 Microsoft.AspNetCore.Identity 的 using，新增 CLOthings_API.Models 的 using，避免重複引用並正確導入所需命名空間。

--- CLOthings_API/Program.cs
+++ CLOthings_API/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
