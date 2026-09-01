101b148c5bf0c2b3503dd001527f52a294d9df43
Tony <asd.46563@gmail.com>
2026-09-01 22:24:19 +0800

    新增 ContactController 並提供查詢 SMTP Email API
    新增 api/contact 路由的控制器，實作 GET api/contact/email 取得設定檔中的 Smtp:Email，若未設定則回傳 NotFound，否則回傳 email。

---
+++ CLOthings_API/Controllers/Smtp/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CLOthings_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ContactController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET api/contact/email
+        [HttpGet("email")]
+        public IActionResult GetEmail()
+        {
+            var email = _configuration["Smtp:Email"];
+            if (string.IsNullOrEmpty(email))
+            {
+                return NotFound("Smtp:Email 尚未設定");
+            }
+            return Ok(new { email });
+        }
+    }
+}
